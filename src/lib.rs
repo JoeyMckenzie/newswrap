cfe92df9f5cc453fcaaf5245c2abd194c0ba8070
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-07-03 17:30:36 -0700

    chore: update doctest, force fail

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,22 @@
 //! [Hacker News](https://news.ycombinator.com/) API bindings for Rust.
 //!
-//! ```no_run
-//! use hacker_rs::client::HackerNewsClient;
+//! ```compile_fail
+//! let client = HackerNewsClient::new();
 //!
-//! #[tokio::main]
-//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//!     // Build your client at the start of your application process
-//!     let client = HackerNewsClient::new();
+//! // Call various endpoints with your client instance
+//! let first_item = client.get_item(69).await?;
+//! dbg!(&first_item);
 //!
-//!     // Call various endpoints with your client instance
-//!     let first_item = client.get_item(69).await?;
-//!     dbg!(&first_item);
+//! // Determine what the item type is
+//! let item_type = first_item.get_item_type();
+//! dbg!(item_type);
 //!
-//!     // Determine what the item type is
-//!     let item_type = first_item.get_item_type();
-//!     dbg!(item_type);
+//! // Check if the item is job
+//! assert!(first_item.is_comment());
 //!
-//!     // Check if the item is job
-//!     assert!(first_item.is_comment());
-//!
-//!     // Retrieve user information
-//!     let user = client.get_user("joeymckenzie").await;
-//!     dbg!(user);
-//!
-//!     Ok(())
-//! }
+//! // Retrieve user information
+//! let user = client.get_user("joeymckenzie").await?;
+//! dbg!(user);
 //! ```
 
 #![forbid(unsafe_code, dead_code)]

c91ce692aeef3c3d425f218e50da6dfbcf7a98b1
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-07-07 23:05:06 -0700

    chore: update failing doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,32 +8,19 @@
 //!     // Build your client at the start of your application process
 //!     let client = HackerNewsClient::new();
 //!
-//!     // Optionally build your client with a configured request timeout
-//!     let _client_with_timeout = HackerNewsClient::new_with_timeout_secs(2);
-//!
 //!     // Call various endpoints with your client instance
-//!     let unknown_item = client.get_item(69).await?;
-//!     dbg!(&unknown_item);
+//!     let generic_item = client.items.get_item(69).await?;
+//!     dbg!(&generic_item);
 //!
 //!     // Determine what the item type is
-//!     let item_type = unknown_item.get_item_type();
+//!     let item_type = generic_item.get_item_type();
 //!     dbg!(item_type);
 //!
 //!     // Check if the item is job
-//!     assert!(unknown_item.is_story());
-//!
-//!     // Conveniently request typed items for known IDs
-//!     let story_item = client.get_story(69).await?;
-//!     dbg!(&story_item);
-//!
-//!     // Get child comments associated to the story
-//!     let comment = client
-//!         .get_item(*story_item.comments.first().unwrap())
-//!         .await?;
-//!     dbg!(comment);
+//!     assert!(generic_item.is_story());
 //!
 //!     // Retrieve user information
-//!     let user = client.get_user("joeymckenzie").await?;
+//!     let user = client.users.get_user("joeymckenzie").await?;
 //!     dbg!(user);
 //!
 //!     Ok(())
